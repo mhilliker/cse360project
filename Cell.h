26b0ae4384842c34adeba499777ba9e44840770b
Allen Nguyen <allendewy@gmail.com>
2014-11-10 14:37:12 -0700

    Functional 16 x 16 Board Generation

--- Cell.h
+++ Cell.h
@@ -5,27 +5,24 @@ using namespace std;
 
 class Cell {
 private:
-	int input;
+	char input;
 	bool locked;
 	bool validRow;
 	bool validCol;
 	bool validBox;
-	vector< int > guesses;
-	vector< int > unwanted;
 
 public:
 	Cell();
-	vector< int > attempted;
+	vector< char > attempted;
+	vector< char > oNotes; // list of numbers user decides are possible to use in cell
+	vector< char > xNotes; // list of numbers user decides cannot be used in cell
 	bool getLocked();
 	void lockCell();
-	int getInput();
-	void setInput(int newInput);
+	char getInput();
+	void setInput(char newInput);
 	void resetCell();
 	void setRowValid(bool);
 	void setColValid(bool);
 	void setBoxValid(bool);
-	bool getRowValid();
-	bool getColValid();
-	bool getBoxValid();
 	bool isValid();
 };
