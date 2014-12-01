bd892ef069148b435de181e00f143dfafc0111d2
mhilliker <mrhilliker@gmail.com>
2014-12-01 01:17:26 -0700

    Update Graphics.h

--- Graphics.h
+++ Graphics.h
@@ -1,43 +1,44 @@
-#pragma once
+#ifndef BOARD_H
+#define BOARD_H
 
-#include <Windows.h>
-#include <d2d1.h>
-#include <dwrite.h>
+#include <iostream>
+#include <vector>
 #include <string>
-#include <stdio.h>
-#include <sstream>
-#include <string>
+#include <random>
+#include <time.h>
+#include "Cell.h"
 
 using namespace std;
 
-class Graphics
-{
+class Board {
 private:
-	ID2D1Factory* factory;//creates resources
-	ID2D1HwndRenderTarget* renderTarget;//place in memory where graphics are being drawn
-	ID2D1SolidColorBrush* brush; //prepares colors
-	IDWriteFactory* writefactory;//prepares text drawing
-	IDWriteTextFormat* textformat;//prepared text format
-	IDWriteTextFormat* smalltextformat;//prepared text format
+	int startTime;
+	double fillPercent;
+	int difficulty;
+	bool enableAI;
+	int gridSize;
+	int score;
+	vector< vector< Cell > > solution;
+	vector< vector< Cell > > gameBoard;
+	bool enforceRules(int row, int col, char newInput, bool aiCheck);
+	void check(int col, int row);
 
-	
 public:
-	Graphics();	//default constructor
-	~Graphics();//default destructor
-
-	//initializes the window
-	bool Init(HWND windowHandle);
-
-	//used to begin and end drawing to screen
-	void BeginDraw(){ renderTarget->BeginDraw(); }
-	void EndDraw(){ renderTarget->EndDraw(); }
-
-	//tools to use to draw on screen
-	void ClearScreen(float r, float g, float b); //clears the screen to the designated color
-	void DrawCircle(float x, float y, float radius, float r, float g, float b, float a); //draws a circle
-	void DrawRect(float x, float y, float width, float height, float r, float g, float b, float a, bool fill);
-	void DrawUI();
-	void DrawString(string text, float x, float y, float width, float height);
-	void DrawSmallerString(string text, float x, float y, float width, float height);
-
+	Board(int level, bool ai, bool test);
+	~Board();
+	void newBoard();
+	void endGame();
+	bool newInput(int row, int col, char newInput);
+	vector< vector< Cell > > getSolution();
+	vector< vector< Cell > > getBoard();
+	void oNote(int col, int row, char note, bool add);
+	bool* getoNote(int col, int row);
+	void xNote(int col, int row, char note, bool add);
+	bool* getxNote(int col, int row);
+	void playAI();
+	//void printSolution(); // for testing
+	//void playBoardRndm(); // for testing
+	//void playBoardReal(); // for testing
 };
+
+#endif
